704da085d6c9334555a3bad007f5b5f9ee6c221d
mtfr1 <mateusfigueiredo@dcc.ufmg.br>
2019-05-30 13:04:59 -0300

    extracao por histograma de cor

--- README.md
+++ README.md
@@ -5,7 +5,15 @@
 ### Extração de Features
 
 - Local Binary Pattern (LBP)
-  - taxa de acerto aproximadamente 90%.
-  - utilizando knn com k = 3.
+  - LBP uniforme, 10 features por imagem.
+  - taxa de acerto de aproximadamente 78%.
+  - utilizando KNN com k = 3.
 - Haralick features
 - Histograma de cores
+  - 256 bins totalizando 768 features (3 canais de cor). 
+  - taxa de acerto de aproximadamente 60%.
+  - utilizando KNN com k = 3.
+
+<https://scikit-image.org/docs/0.7.0/api/skimage.feature.texture.html>
+<https://scikit-learn.org/stable/modules/neighbors.html>
+<https://scikit-image.org/docs/dev/api/skimage.exposure.html#skimage.exposure.histogram>
--- confusion_matrix_script/plot_heatmap.py
+++ confusion_matrix_script/plot_heatmap.py
@@ -4,17 +4,11 @@ import seaborn as sns
 
 sns.set()
 
-FILENAME = "input.csv"
-
-data = np.loadtxt(FILENAME, delimiter = ';')
-
 desc = ['Diamond Peach', 'Kiwi', 'Nectarine', 'Asterix Potato', 'Onion', 'Honneydew Melon', 'Taiti Lime', 'Spanish Pear', 'Cashew', 'Fuji Apple', 'Watermelon', 'Orange', 'Granny Smith Apple', 'Plum', 'Agata Potato']
-
-
-plt.figure(figsize = (16,5))
-
-sns.heatmap(data, xticklabels=desc, yticklabels=desc, linewidths=.5, annot=True)
-
-
-plt.savefig(FILENAME + '.pdf', bbox_inches='tight')
-plt.show()
+files = ["lbp", "hist"]
+for i in files:	
+	filename = "input_" + i + ".csv"
+	data = np.loadtxt(filename, delimiter = ';')
+	plt.figure(figsize = (16,5))
+	sns.heatmap(data, xticklabels=desc, yticklabels=desc, linewidths=.5, annot=True)
+	plt.savefig(filename + '.pdf', bbox_inches='tight')
